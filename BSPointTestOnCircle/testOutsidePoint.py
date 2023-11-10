d63d8a6cb810c3786baa9b45d42a26d4f58b2be2
Daniel Krawciw <daniel.krawciw@gmail.com>
2023-11-10 15:11:26 -0700

    Making the other testcase where the particle starts outside of the ring

---
+++ BSPointTestOnCircle/testOutsidePoint.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from TestUtils.circleTest import circleTest
+
+# Calculate points on the circle to plot
+circle = lambda t : [np.cos(t), np.sin(t), t * 0]
+circlePoints = np.linspace(0,2*np.pi,1000)
+s = np.array(circle(circlePoints))
+
+# Set up some values as initial conditions
+TOTAL_TIME = 100
+TOTAL_STEPS = 1000
+IV = np.array( [2,0,0] )
+
+# Simulate the point
+pts = circleTest(TOTAL_TIME, TOTAL_STEPS, IV)
+
+# Plot the points around the circle
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot(s[0,:],s[1,:],s[2,:])
+ax.plot(pts[0,:],pts[1,:],pts[2,:])
+
+plt.show()
