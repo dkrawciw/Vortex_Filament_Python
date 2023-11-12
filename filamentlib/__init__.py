f42ba878c679f03070a8929e92fa5a66e56ef821
Daniel Krawciw <daniel.krawciw@gmail.com>
2023-11-11 18:39:04 -0700

    Reorganized the functions to be easily accessable by other files in the project

---
+++ filamentlib/__init__.py
@@ -0,0 +1,1 @@
+from filamentlib.filament import VField
---
+++ filamentlib/filament/VField.py
@@ -0,0 +1,18 @@
+import numpy as np
+class VField():
+# Define various methods of calculating the velocity field at a point
+
+# BiotSavart is the general way of calculating the velocity field at a point
+    @staticmethod
+    def BiotSavart(curve, curveTangent, curvePoint):
+        s = range(0,len(curve[0]))
+        dv = []
+        for c in s:
+            num = np.cross( curveTangent[:,c], curvePoint - curve[:,c] )
+            den = np.linalg.norm(curvePoint - curve[:,c]) ** 3
+            dv.append( num/den )
+
+        dv = np.array(dv)
+
+        v = np.trapz([dv[:,0],dv[:,1],dv[:,2]], s)
+        return v
