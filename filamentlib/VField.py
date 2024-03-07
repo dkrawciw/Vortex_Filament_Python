7172be10b9a3a54f902392a3d7c39e8acc8e88f1
Daniel Krawciw <daniel.krawciw@gmail.com>
2024-03-07 16:30:43 -0700

    Added formula for calculating the field over closed curves

--- filamentlib/VField.py
+++ filamentlib/VField.py
@@ -1,7 +1,21 @@
 import numpy as np
 
 class VField():
-# Define various methods of calculating the velocity field at a point
+    # Define various methods of calculating the velocity field at a point
+    def closedGradient(curve: np.array):
+
+        shiftDist = round(curve.shape[1]/2)
+
+        grad = np.gradient(curve)[1]
+        shiftedCurve = np.roll(curve,shiftDist,axis=1)
+        shiftedGrad = np.gradient(shiftedCurve)[1]
+
+        setBound = round(shiftDist/2)
+
+        grad[:,-setBound-1:-1] = shiftedGrad[:,shiftDist - setBound:shiftDist]
+        grad[:,0:setBound] = shiftedGrad[:, shiftDist : shiftDist + setBound]
+
+        return grad
 
     #BiotSavart is the general way of calculating the velocity field at some given points over a curve with given tangent points
     @staticmethod
@@ -46,6 +60,19 @@ class VField():
     def KappaBinormal( curve: np.array ):
         curveTangent = np.gradient(curve)[1]
         curveNormal = np.gradient(curveTangent)[1]
+        curveBinormal = np.cross(curveTangent,curveNormal, axisa=0, axisb=0, axisc=0)
+
+        Kappa = np.divide( np.linalg.norm( curveBinormal ), np.power( np.linalg.norm(curveTangent),3 ) )
+
+        KBApprox = np.multiply(Kappa, curveBinormal)
+
+        return KBApprox
+    
+    # Approximating Closed Shapes
+    @staticmethod
+    def KappaBinormalClosed( curve: np.array ):
+        curveTangent = VField.closedGradient(curve)
+        curveNormal = VField.closedGradient(curveTangent)
         curveBinormal = np.cross(curveTangent,curveNormal, axisa=0, axisb=0, axisc=0)
 
         Kappa = np.divide( np.linalg.norm( curveBinormal ), np.power( np.linalg.norm(curveTangent),3 ) )
